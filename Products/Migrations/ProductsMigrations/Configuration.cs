2bb71b18e72e80172d9f126980b84cc542755852
Paul <Paul@VAIO>
2017-12-12 13:25:25 +0000

    Part 3 Seeding Data Complete

--- Products/Migrations/ProductsMigrations/Configuration.cs
+++ Products/Migrations/ProductsMigrations/Configuration.cs
@@ -16,8 +16,43 @@ namespace Products.Migrations.ProductsMigrations
 
         protected override void Seed(Products.Models.ProductDbContext context)
         {
-        
+            SeedCategoryProducts(context);
+            SeedProductCategory(context);
+
         }
-        
+        private void SeedCategoryProducts(ProductDbContext context)
+        {
+            context.Categories.AddOrUpdate(c => c.CategoryName,
+                new Category[]
+                {
+                    new Category
+                    {
+                         CategoryName = "Plumbing",
+                          productsInCategory = new Product[]
+                          {
+                              new Product { Description = "stopper",
+                                             FirstStockedOn = DateTime.Now - new TimeSpan(364,2,0,0),
+                                               LastOrderDate = DateTime.Now - new TimeSpan(5,0,0),
+                                                QuantityInStock = 200,
+                                                 UnitPrice = 0.20f
+                              }
+                          }
+                    }
+                }
+                );
+        }
+        private void SeedProductCategory(ProductDbContext context)
+        {
+            context.Products.AddOrUpdate(p => p.Description,
+                new Product[] {
+                    new Product { Description="9 inch Bolts",
+                                  QuantityInStock = 200, UnitPrice = 0.50f,
+                  FirstStockedOn = DateTime.Now - new TimeSpan(640,2,0,0),
+                                  LastOrderDate = DateTime.Now - new TimeSpan(3,0,0),
+                                  associatedCategory = new Category { CategoryName = "Building Supplies" },
+                                        }
+                                });
+
+        }
     }
 }
